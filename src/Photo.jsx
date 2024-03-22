b7d87825196278ec5b9453b8ba468edf72d4deb1
Johan Söderlund <johan.soderlund96@gmail.com>
2024-03-22 22:55:54 +0100

    styling for text and photo

--- src/Photo.jsx
+++ src/Photo.jsx
@@ -3,9 +3,13 @@ import photoGrid from "./assets/grid.png";
 
 const Photo = () => {
   return (
-    <main>
-      <h1>test</h1>
+    <main className="photo-container">
       <img className="photo-grid" src={photoGrid} alt="grid of photos" />
+      <h1 className="grid-headline">Online Experiences</h1>
+      <p className="grid-paragraph">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
     </main>
   );
 };
